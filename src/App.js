6246013a616cae72e8e3a511a6825d49e7fce38d
James Haggerty <jpm.haggerty@gmail.com>
2021-10-14 17:20:32 -0400

    Halfway through

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Link, Switch } from "react-router-dom";
+import "./App.css";
+import { Navbar, Container, Nav } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand>
+            <Nav className="me-auto">
+              <button>
+                <Link to="/">Home</Link>
+              </button>
+              <button>
+                <Link to="/products">Products</Link>
+              </button>
+              <button>
+                <Link to="/checkout">Checkout</Link>
+              </button>
+            </Nav>
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+
+      <Switch>
+        <Route
+          exact
+          path="/products/item1"
+          render={(props) => <Link to="/cart">Item One</Link>}
+        />
+        <Route
+          exact
+          path="/products/item2"
+          render={(props) => <Link to="/cart">Item Two</Link>}
+        />
+        <Route
+          exact
+          path="/products/item3"
+          render={(props) => <Link to="/cart">Item Three</Link>}
+        />
+      </Switch>
+
+      <button>
+        <Link to="/products/item1">Item One</Link>
+      </button>
+      <button>
+        <Link to="/products/item2">Item Two</Link>
+      </button>
+      <button>
+        <Link to="/products/item3">Item Three</Link>
+      </button>
+
+      <Route
+        path="/checkout"
+        render={(props) => <Link to="/checkout">Cart</Link>}
+      />
+      <Route
+        path="/cart"
+        render={(props) => <Link to="/">Complete Purchase</Link>}
+      />
     </div>
   );
 }
